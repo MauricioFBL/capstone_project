86b9c430d62d7c3ef24dfcd7dd776735eca91853
Mauricio Bautista <mauxyugi@hotmail.com>
2022-07-16 13:46:17 -0500

    Updated file to setup infraestructure

---
+++ set_infraestructure.sh
@@ -0,0 +1,16 @@
+cd infraestructure/aws
+terraform init
+terraform validate 
+terraform apply --var-file=terraform.tfvars 
+aws eks --region $(terraform output -raw region) update-kubeconfig --name $(terraform output -raw cluster_name)
+export NFS_SERVER=$(terraform output -raw efs)
+cd ../kubernetes
+kubectl create namespace storage
+helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
+    --namespace storage \
+    --set nfs.server=$NFS_SERVER \
+    --set nfs.path=/
+kubectl create namespace airflow
+helm repo add apache-airflow https://airflow.apache.org
+helm install airflow -f airflow-values.yaml apache-airflow/airflow --namespace airflow
+kubectl get pods -n airflow
