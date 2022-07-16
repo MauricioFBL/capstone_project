6446abcbcbcfcd01cd6969cb18ef5a442f0e383d
Mauricio Bautista <mauxyugi@hotmail.com>
2022-07-16 14:11:55 -0500

    Updated dag

--- dags/s3_2_postge.py
+++ dags/s3_2_postge.py
@@ -16,7 +16,7 @@ CLOUD_PROVIDER = "aws"
 
 # AWS constants
 AWS_CONN_ID = "s3_conn"
-S3_BUCKET_NAME = "s3-data-bootcamp-elchemarug0102003"
+S3_BUCKET_NAME = "s3-data-bootcamp-maufbl0808109231"
 S3_KEY_NAME = "user_purchase.csv"
 
 # Postgres constants
